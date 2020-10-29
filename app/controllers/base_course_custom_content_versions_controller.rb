966b404fdb87519a6842f1f711c73f88d7be2882
Brian Sadler <sadler.brian@gmail.com>
2020-10-29 15:20:22 -0400

    More fixing of specs and not breaking the old stuff.
    We're going to do a followup PR with the core logic of
    create, update, delete for a BaseCourseCustomContentVersion.
    
    Specs are still failing. Decision is to rip out anything failing related
    to New or Edit of a Course. But we need to figure out what to do with
    New Course Template related failing specs.

--- app/controllers/base_course_custom_content_versions_controller.rb
+++ app/controllers/base_course_custom_content_versions_controller.rb
@@ -9,9 +9,12 @@ class BaseCourseCustomContentVersionsController < ApplicationController
   nested_resource_of BaseCourse
 
   before_action :set_custom_content, only: [:create]
-  before_action :set_base_course # TODO: shouldn't DryCrud handle this?
+  before_action :set_base_course, except: [:create] # TODO: shouldn't DryCrud handle this?
 
+  # TODO: get rid of these when we refactor to be run from Course Mgmt page.
+  before_action :set_lti_launch, only: [:create]
   skip_before_action :verify_authenticity_token, only: [:create], if: :is_sessionless_lti_launch?
+  before_action :set_base_course_from_lti_launch, only: [:create]
 
   # Show form to select new Project to create as an LTI linked Canvas assignment
   def new
@@ -26,11 +29,27 @@ class BaseCourseCustomContentVersionsController < ApplicationController
   # Create a Project for an LTI assignment placement
   def create
 
+    # TODO: refactor this to be run from the Course Mgmt page. Create a new assignment 
+    # in Canvas through the API, save a new custom_content_version and create a new 
+    # BaseCourseCustomContentVersion with it mapping this base course to the new canvas assignment
+    # and content version
+
     # If we end up adding a designer role, remember to authorize `ProjectVersion.create?`.
     authorize BaseCourseCustomContentVersion
+    # Create new version of the content
+    @custom_content.save_version!(current_user)
 
-    raise NotImplementedError, "TODO: save a new custom_content_version and create a new BaseCourseCustomContentVersion with it mapping it to this BaseCourse[#{@base_course.inspect}]"
-   
+    # Create join table entry
+    @course_content_version = BaseCourseCustomContentVersion.create!(
+      base_course: @base_course,
+      custom_content_version: @custom_content.last_version,
+    )
+
+    # Create a submission URL for this course and version
+    submission_url = new_base_course_custom_content_version_project_submission_url(
+      base_course_custom_content_version_id: @course_content_version.id,
+    )
+    @deep_link_return_url, @jwt_response = helpers.lti_deep_link_response_message(@lti_launch, submission_url)
   end
 
   # Publish the latest Project, Survey, etc (aka CustomContent) so that the Canvas assignment this
@@ -61,4 +80,9 @@ class BaseCourseCustomContentVersionsController < ApplicationController
     @base_course.verify_can_edit!
   end
 
+ def set_base_course_from_lti_launch
+    @base_course = BaseCourse.find_by!(
+      canvas_course_id: @lti_launch.request_message.custom['course_id'],
+    )
+  end
 end
