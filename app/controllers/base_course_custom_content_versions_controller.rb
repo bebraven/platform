982ea6f41fad128a9b08347fe86243b05e71e102
Brian Sadler <sadler.brian@gmail.com>
2020-10-29 13:56:12 -0400

    Add the UI for creating, updating, and deleting Projects and Surveys.
    1. Adds a list of Projects and Surveys to the BaseCourse edit page.
    2. Clicking "Add Project" or "Add Survey" shows the list of Projects or Surveys to choose from.
    3. Clicking "Create" on the edit page calls the create action on base_course_custom_content_versions (not implemented yet)
    4. Clicking on the name of an existing project shows the published version contents in Canvas.
    5. Clicking "Publish Latest" on a project calls PUT on the existing BaseCourseContentVersion record (not implemented yet)
    6. Clicking "Delete" on a project calls DELETE on the existing BaseCourseContentVersion record (not implemented yet)
    7. Clicking on the name of a Course or CourseTemplate from the main page shows a read-only view of
       everything on the Edit page.
    8. Only a CourseTemplate shows the button to Edit
    9. If you URL hack the edit routes for a course, an exception is thrown.

--- app/controllers/base_course_custom_content_versions_controller.rb
+++ app/controllers/base_course_custom_content_versions_controller.rb
@@ -6,30 +6,47 @@ class BaseCourseCustomContentVersionsController < ApplicationController
 
   layout 'lti_placement'
 
-  before_action :set_lti_launch, only: [:create]
+  nested_resource_of BaseCourse
+
   before_action :set_custom_content, only: [:create]
-  before_action :set_base_course, only: [:create]
+  before_action :set_base_course # TODO: shouldn't DryCrud handle this?
 
   skip_before_action :verify_authenticity_token, only: [:create], if: :is_sessionless_lti_launch?
 
+  # Show form to select new Project to create as an LTI linked Canvas assignment
+  def new
+    # If we end up adding a designer role, remember to authorize `ProjectVersion.create?`.
+    authorize @base_course_custom_content_version
+
+    # TODO: exclude those already on this BaseCourse.
+    @projects = Project.all
+  end
+
   # Create a Project for an LTI assignment placement
   def create
+
     # If we end up adding a designer role, remember to authorize `ProjectVersion.create?`.
     authorize BaseCourseCustomContentVersion
-    # Create new version of the content
-    @custom_content.save_version!(current_user)
 
-    # Create join table entry
-    @course_content_version = BaseCourseCustomContentVersion.create!(
-      base_course: @base_course,
-      custom_content_version: @custom_content.last_version,
-    )
+    raise NotImplementedError, "TODO: save a new custom_content_version and create a new BaseCourseCustomContentVersion with it mapping it to this BaseCourse[#{@base_course.inspect}]"
+   
+  end
 
-    # Create a submission URL for this course and version
-    submission_url = new_base_course_custom_content_version_project_submission_url(
-      base_course_custom_content_version_id: @course_content_version.id,
-    )
-    @deep_link_return_url, @jwt_response = helpers.lti_deep_link_response_message(@lti_launch, submission_url)
+  # Publish the latest Project, Survey, etc (aka CustomContent) so that the Canvas assignment this
+  # BaseCourseCustomContentVersion represents shows the latest content.
+  def update
+    authorize @base_course_custom_content_version
+
+    raise NotImplementedError, "TODO: save a new custom_content_version from the latest and re-associated this BaseCourseCustomContentVersion[#{@base_course_custom_content_version.inspect}] with it. Canvas assignment doesn't need to change."
+  end
+
+  # Deletes a Project, Survey, etc (aka CustomContent) from the Canvas course that this
+  # BaseCourseCustomContentVersion join model represents and then deletes this record locally.
+  def destroy
+    authorize @base_course_custom_content_version
+
+    raise NotImplementedError, "TODO: delete both the Canvas assignment and this BaseCourseCustomContentVersion[#{@base_course_custom_content_version.inspect}]"
+
   end
 
   private
@@ -39,8 +56,8 @@ class BaseCourseCustomContentVersionsController < ApplicationController
   end
 
   def set_base_course
-    @base_course = BaseCourse.find_by!(
-      canvas_course_id: @lti_launch.request_message.custom['course_id'],
-    )
+    @base_course = BaseCourse.find(params.require(:base_course_id))
+    @base_course.verify_can_edit!
   end
+
 end
