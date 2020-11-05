0a8949d7605dac8e87d5b20906a3893b6c4c606a
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2020-11-05 19:26:26 +0100

    Dry up some controllers

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -4,30 +4,29 @@
 class SlackController < ApplicationController
   layout 'admin'
 
+  before_action :authorize_index
+
   def init_sync_to_slack
-    authorize :application, :index?
     @show_booster_slack_sync = booster_instance?
   end
 
-  def init_sync_to_booster_slack
-    authorize :application, :index?
-  end
-
   def sync_to_booster_slack
-    authorize :application, :index?
     redirect_to root_path, notice: 'Nothing happened! Run this on booster platform instead' and return unless booster_instance?
 
     email = params[:email]
     emails = params[:emails]
+
     SyncToBoosterSlackJob.perform_later(emails, email)
+
     redirect_to root_path, notice: 'The sync process was started. Watch out for an email'
   end
 
   def sync_to_slack
-    authorize :application, :index?
     program_id = params[:program_id]
     email = params[:email]
+
     SyncToSlackJob.perform_later(program_id, email)
+
     redirect_to root_path, notice: 'The sync process was started. Watch out for an email'
   end
 
@@ -35,5 +34,9 @@ class SlackController < ApplicationController
 
   def booster_instance?
     ENV['BOOSTER_INSTANCE'].present?
+  end
+
+  def authorize_index
+    authorize :application, :index?
   end
 end
--- app/controllers/webinar_controller.rb
+++ app/controllers/webinar_controller.rb
@@ -1,36 +1,37 @@
 # frozen_string_literal: true
-#
+
 require 'csv'
 
 # Webinar controller
 class WebinarController < ApplicationController
   layout 'admin'
 
-  def init_sync_to_webinar
-    authorize :application, :index?
-  end
-
-  def init_generate_webinar
-    authorize :application, :index?
-  end
+  before_action :authorize_index
 
   def generate_webinar
-    authorize :application, :index?
     participants_file = params[:participants]
-    participants = CSV.read(participants_file.path, headers: true).map(&:to_h)
     email = params[:email]
     meeting_id = params[:meeting_id]
+
+    participants = CSV.read(participants_file.path, headers: true).map(&:to_h)
     GenerateWebinarJob.perform_later(meeting_id, email, participants)
+
     redirect_to root_path, notice: 'The generation process was started. Watch out for an email'
   end
 
   def sync_to_webinar
-    authorize :application, :index?
     program_id = params[:program_id]
     email = params[:email]
-    force_update = params[:force_update].eql?("1")
+    force_update = params[:force_update].eql?('1')
+
     SyncToWebinarJob.perform_later(program_id, email, force_update)
+
     redirect_to root_path, notice: 'The sync process was started. Watch out for an email'
   end
 
+  private
+
+  def authorize_index
+    authorize :application, :index?
+  end
 end
