6e8cf5cf4399d232b4d2ba76b61b380324c857e8
Rowan Tatai <20307391+Rowantatai@users.noreply.github.com>
2021-01-27 15:51:12 -0500

    Redirect non admin users to the portal (#565)
    https://github.com/bebraven/platform/pull/565

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,12 @@
 class HomeController < ApplicationController
   layout 'admin'
 
+  before_action :admin_only
+
   def welcome
     authorize :application, :index?
 
-    @links = [ 
+    @links = [
       { title: 'Portal', path: canvas_url, blank_target: true, is_active: true},
       { title: 'Content Editor', path: new_custom_content_path },
       { title: 'Course Management', path: base_courses_path },
@@ -14,5 +16,11 @@ class HomeController < ApplicationController
       { title: 'Sync To Webinar', path: sync_to_webinar_path },
       { title: 'Users', path: users_path }
     ]
+  end
+
+  private
+
+  def admin_only
+    redirect_to :canvas unless current_user.admin?
   end
 end
