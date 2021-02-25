c74439cd9156454197650d1eba74de95e0cd306b
rshipp <github@rshipp.com>
2021-02-25 15:39:29 -0600

    more controller work

--- app/controllers/peer_review_results_controller.rb
+++ app/controllers/peer_review_results_controller.rb
@@ -5,25 +5,75 @@ class PeerReviewResultsController < ApplicationController
 
   nested_resource_of Course
 
-  before_action :set_peer_results, only: [:show]
+  before_action :set_course, only: [:launch]
 
+  # Read from LTI payload, redirect to appropriate #show path.
   def launch
+    authorize @peer_review_result
+
+    redirect_to peer_review_results_path(@peer_review_result, state: @lti_launch.state) and return if @peer_review_result
+
+    # TODO
+    render plain: "No results yet"
   end
 
+  # Admin-only, nested under course.
+  def index
+    authorize @peer_review_results
+
+    @peer_review_submissions = peer_review_submissions
+  end
+
+  # POST
+  # Admin-only, nested under course.
+  def score
+    authorize peer_review_submissions
+    transaction do
+      peer_review_submissions.each do |submission|
+        submission.update!(new: false)
+      end
+    end
+  end
+
+  # Show results and score breakdown.
   def show
+    authorize @peer_review_result
+
+    # Initialize to zero.
+    @question_scores = {}
+    PeerReviewQuestion.each do |question|
+      @question_scores[question.text] = 0.0
+    end
+
+    # Total by summation.
+    @peer_review_submission_answers.each do |answer|
+      @question_scores[answer.question.text] += answer.input_value.to_f
+    end
+
+    # Divide to get the mean.
+    @question_scores.each do |k, v|
+      # Note submission count is possible ONLY because of the uniqueness constraint on (user,course).
+      @question_scores[k] = @question_scores[k] / peer_review_submissions.count
+    end
   end
 
 private
-  def set_peer_results
-    # Get the section in this course that the user is enrolled in as a student
-    student_section = current_user.student_section_by_course(@course)
-    if student_section&.students
-      # You're enrolled as a student and there are other students in this course
-      @peer_users = student_section.students.where.not(id: current_user.id)
-    else
-      # You're not enrolled as a student e.g., you're a TA or admin, or there
-      # are no other students in this course
-      @peer_users = []
-    end
+  def set_course
+    @course = Course.find_by!(canvas_course_id: @lti_launch.request_message.canvas_course_id)
+  end
+
+  # TODO: Need correct unique constraints on questions/answers.
+  def peer_review_submissions
+    PeerReviewSubmission.where(
+      course: @course,
+      new: false,
+    )
+  end
+
+  def peer_review_submission_answers
+    PeerReviewSubmissionAnswer.where(
+      submission: peer_review_submissions,
+      for_user: current_user,
+    )
   end
 end
