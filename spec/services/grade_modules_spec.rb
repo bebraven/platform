fefb9be931cf1fe2765d199107690c7505dfa1e8
rshipp <github@rshipp.com>
2021-03-10 11:43:49 -0600

    Add empty service spec

---
+++ spec/services/grade_modules_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe GradeModules do
+
+  describe "#run"
+  end  # run
+
+  describe "#grade_course" do
+  end  # grade_course
+
+  describe "#grade_assignment" do
+  end  # grade_assignment
+end
